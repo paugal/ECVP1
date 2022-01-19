b523e5cc60b89acf4afe6ee533e3faf3cbebf7a4
paugal <pau.galan22@gmail.com>
2022-01-19 20:58:58 +0100

    update
    rooms

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
+
+//VARIABLES GLOBALES
 var server = null;
 var location_server = "wss://ecv-etic.upf.edu/node/9000/ws";
-var room_name = "PauGGRoom";
+var room_name = "PauGGRoom/";
+var selectedRoom = "Principal";
 var userId = '';
 var userName = '';
-
+var users = [
+	{
+		userid: '0',
+		username: 'pauGalan'
+	}
+]
 var msgs =[
 	{
 		type: 'text',
@@ -11,10 +19,25 @@ var msgs =[
 		username: 'paugal'
 	}
 ];
-//connect to the server
+
+var msg = {
+	type: "history",
+	content: [
+		{
+			type: "text",
+			username: "foo",
+			content: "..."
+		}		
+	]
+};
+
+
+
+//INICIO DE CONECXION
 server = new SillyClient();
-server.connect( location_server , room_name);
-//this method is called when the user gets connected to the server
+
+
+
 server.on_ready = function( my_id )
 {
 	console.log(my_id);
@@ -22,45 +45,91 @@ server.on_ready = function( my_id )
 }
 
 server.on_connect = function( server ){
-    
 };
+function selectRoom(roomName){
 
-function onMessageReceived( author_id, str_msg )
-{
-  displayMsg(str_msg, author_id);
 }
 
-server.on_message = onMessageReceived;
-
 function enterChat(){
-	const validation = document.getElementById("userid");
+	const validation = document.getElementById("useridInput");
 	if (validation.checkValidity()) {
 		var loginBox = document.getElementById("loginBox");
 		var chatBox = document.getElementById("allApp");
 
 		loginBox.style.display = 'none';
 		chatBox.style.display = 'inline';
+		try {
+			selectedRoom = document.querySelector('input[name="roomselector"]:checked').value;
+			server.connect( location_server , room_name + selectedRoom);
+		} catch (error) {
+			selectedRoom = 'Principal';
+			server.connect( location_server , room_name + selectedRoom);
+		}
+		setNameTitle();
+		
 	}
 }
 
-function getUserId(){
-	userName =  document.getElementById("userid").value;
-	console.log(userId);
+function getUserName(){
+	userName =  document.getElementById("useridInput").value;
+	enterChatMsg()
 }
 
 function setNameTitle(){
-	document.getElementById("nameTitle").innerHTML = room_name +'/' + userName;
+	console.log(selectedRoom)
+	document.getElementById("nameTitle").innerHTML = selectedRoom;
 }
 
+function enterChatMsg(){
+	var enterMsg = {type: 'enter', username: userName,content: null}
+	var msg_str = JSON.stringify( enterMsg );
+	server.sendMessage(msg_str);
+}
+
+//RECEPCION DE MENSAJES
+server.on_message = onMessageReceived;
+
+function onMessageReceived( authorId, data )
+{
+	var msgData = JSON.parse(data);
+	if(msgData.type == 'text'){
+		receiveTextMsg(msgData);
+	}else if(msgData.type == 'enter'){
+		newUserChat(msgData);
+	}
+	
+}
+
+function receiveTextMsg(msgData){
+	displayMsg(msgData.username, msgData.content);
+}
+
+function newUserChat(msgData){
+	var div = document.createElement("div");
+	div.classList.add('chattext');
+	div.classList.add('enter');
+	div.textContent = "El usuario " + msgData.username + " acaba de conertarse.";
+	document.getElementById("chat").appendChild(div);
+
+}
+
+
+//ENVIO DE MENSAJES
 function pushMsg(){
 	var msgtext = document.getElementById("inputMsg").value;
-	msgs.push(['text', msgtext,userName]);
+	var msgAux = {type:'text', username: userName, content: msgtext};
+	msgs.push(msgAux);
 	document.getElementById("inputMsg").value = '';
-	displayMsg(msgtext, userName);
-	server.sendMessage(msgtext);
+
+	//mostramos nuestro mensaje
+	displayMsg(userName, msgtext);
+
+	//Convertimos el objeto en string
+	var msg_str = JSON.stringify( msgAux );
+	server.sendMessage(msg_str);
 }
 
-function displayMsg(msgtext, userNameMsg){
+function displayMsg(userNameMsg, msgtext){
 	var div = document.createElement("div");
 	div.classList.add('chattext');
 	if( userNameMsg == userName){
@@ -68,7 +137,31 @@ function displayMsg(msgtext, userNameMsg){
 		div.textContent = msgtext;
 	}else{
 		div.classList.add('receive');
-		div.textContent = userName + ": " + msgtext;
+		div.textContent = userNameMsg + ": " + msgtext;
 	}
 	document.getElementById("chat").appendChild(div);
+}
+
+
+
+
+//EXTRAS
+
+
+//FUNCIONES PARA PODER ENVIAR CON ENTER EN LAS CAJAS DE TEXTO
+function ClientOnTyping(inputElement, elementBtn){
+	console.log('Typing...');
+	var input = document.getElementById(inputElement);
+	// Execute a function when the user releases a key on the keyboard
+	input.addEventListener("keyup", buttonWithEnter(event, elementBtn));
+}
+
+function buttonWithEnter(event, elementBtn) {
+	// Number 13 is the "Enter" key on the keyboard
+	if (event.keyCode === 13) {
+	  // Cancel the default action, if needed
+	  event.preventDefault();
+	  // Trigger the button element with a click
+	  document.getElementById(elementBtn).onclick();
+	}
 }
